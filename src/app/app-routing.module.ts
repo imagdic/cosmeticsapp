5764eec2c707552c85263a52f9a602084f6f3c71
iva.magdic <iva.magdic@gdi.net>
2023-08-17 14:42:15 +0200

    product details design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { Wishlist } from './shared/wishlist';
+import { WishlistPageComponent } from './wishlist-page/wishlist-page.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
-  {path:'product/:id', component: ProductDetailsComponent}
+  {path:'product/:id', component: ProductDetailsComponent},
+  {path:'wishlist', component: WishlistPageComponent}
 ];
 
 @NgModule({
