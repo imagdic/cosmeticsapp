8a988c9aa1564e383c8db9c342786e8876f706f7
iva.magdic <iva.magdic@gdi.net>
2023-08-18 15:57:25 +0200

    login & register component created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,15 @@ import { HomeComponent } from './home/home.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { Wishlist } from './shared/wishlist';
 import { WishlistPageComponent } from './wishlist-page/wishlist-page.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
   {path:'product/:id', component: ProductDetailsComponent},
-  {path:'wishlist', component: WishlistPageComponent}
+  {path:'wishlist', component: WishlistPageComponent},
+  {path:'login', component: LoginComponent},
+  {path:'register', component: RegistrationComponent}
 ];
 
 @NgModule({
