3e65028823c1338472b8ceaca68c838cdde25123
iva.magdic <iva.magdic@gdi.net>
2023-09-05 17:42:30 +0200

    unneccessary comments deleted

--- src/app/rating/rating.component.ts
+++ src/app/rating/rating.component.ts
@@ -19,30 +19,11 @@ export class RatingComponent implements OnInit {
 
 
   rateProduct(event: any) {
-    const productId = 'your-product-id'; // Replace with the actual product ID
-    const rating = event.value; // The selected rating value from the event
+    const productId = 'your-product-id';
+    const rating = event.value; 
 
-    // Call the addRating method from your RatingService
     this.ratingService.addRating(productId, rating);
   }
 
-
-  // onStarClick(starValue: number) {
-  //   if (!this.user) {
-  //     // User is not authenticated, show a message or a popup.
-  //     alert('Please log in to leave a review.');
-  //     return;
-  //   }
-
-  //   if (!this.currentProduct) {
-  //     // Handle the case where there's no current product selected.
-  //     return;
-  //   }
-
-  //   // User is authenticated, allow rating.
-  //   this.selectedRating = starValue;
-  //   // Emit the rating to the parent component with product information.
-  //   this.ratingSubmitted.emit({ productId: this.currentProduct.id, rating: this.selectedRating });
-  // }
 
 }
