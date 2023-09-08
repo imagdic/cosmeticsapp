896a4d65f79011eb2224457e24f70443dd99c1e2
iva.magdic <iva.magdic@gdi.net>
2023-09-08 09:58:12 +0200

    wishlist page component finished

--- src/app/wishlist-page/wishlist-page.component.ts
+++ src/app/wishlist-page/wishlist-page.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { WishlistService } from '../services/wishlist.service';
 import { Products } from '../shared/products';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { RatingService } from '../services/rating.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-wishlist-page',
@@ -10,12 +13,36 @@ import { Observable } from 'rxjs';
 })
 export class WishlistPageComponent implements OnInit {
   wishlistProducts: Products[] = [];
+  averageRatings: { [key: string]: number | null } = {};
 
-  constructor(private wishlistService: WishlistService) { }
-  ngOnInit() {
-    this.wishlistService.getUserWishlistProducts().subscribe(products => {
-      this.wishlistProducts = products;
-    })
+  constructor(private wishlistService: WishlistService, private authService: AuthService, private ratingService: RatingService,
+    private router: Router) { }
+    ngOnInit() {
+      this.wishlistService.getUserWishlistProducts().subscribe(products => {
+        this.wishlistProducts = products;
+        products.forEach(product => {
+          this.ratingService.getAverageProductRating(product.id).subscribe(avgRating => {
+            this.averageRatings[product.id] = avgRating;
+          });
+        });
+      });
+    }
+    
+
+  rateProduct(productId: string, ratingValue: number): void {
+    console.log('Rating product with ID:', productId);
+    this.authService.isAuthenticated().subscribe(isAuthenticated => {
+      if (!isAuthenticated) {
+        alert('Please log in to leave a review!');
+        return;
+      }
+      this.ratingService.rateProduct(productId, ratingValue);
+      console.log(productId)
+    });
+  }
+
+  viewProductDetails(productId: string) {
+    this.router.navigate(['/products', productId]);
   }
 
 }
