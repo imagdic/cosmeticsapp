88c367f505d8048a43a42891e87eb26627b5c3c3
iva.magdic <iva.magdic@gdi.net>
2023-09-10 20:07:42 +0200

    changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,7 @@ export const environment = {
     projectId: 'cosmeticsapp-e222f',
     appId: '1:1080849464711:web:1dbcb29a6567ae468c2722',
     storageBucket: 'cosmeticsapp-e222f.appspot.com',
+    locationId: 'europe-west',
     apiKey: 'AIzaSyBlRbvoVGTQrgSuLifHL8nsKXGMF983XAc',
     authDomain: 'cosmeticsapp-e222f.firebaseapp.com',
     messagingSenderId: '1080849464711',
